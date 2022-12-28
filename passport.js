f6240c1bf272d038132ec41a1ffb18b6b6c76c4a
Luisa Ihle <luisa.ihle@gmail.com>
2022-12-28 17:55:30 +0100

    update passport.js

--- passport.js
+++ passport.js
@@ -24,12 +24,6 @@ passport.use(new LocalStrategy({
       console.log('incorrect username');
       return callback(null, false, {message: 'Incorrect username.'});
     }
-   
-    // to validate any  password a user enters
-    if (!user.validatePassword(password)) {
-      console.log('incorrect password');
-      return callback(null, false, {message: 'Incorrect password.'});
-    }
 
     console.log('finished');
     return callback(null, user);
