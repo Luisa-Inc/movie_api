366a5008c5e0f4dc122860d160b6fbbc3ff1d588
Luisa Ihle <luisa.ihle@gmail.com>
2022-12-28 17:54:31 +0100

    update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const Models = require('./models.js');
+const { check, validationResult } = require('express-validator');
 
 const Movies = Models.Movie; //refer to the model names you defined in “models.js”
 const Users = Models.User; //refer to the model names you defined in “models.js”
@@ -19,6 +20,11 @@ mongoose.connect('mongodb://localhost:27017/myFlixDB ', { useNewUrlParser: true,
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const cors = require('cors');
+app.use(cors());
+
+/* rest of code goes here*/
 
 let auth = require('./auth')(app);
 
@@ -37,41 +43,47 @@ app.get('/', (req, res) => {
 });
 
 //Add a user / CREATE
+app.post('/users', 
+[ 
+  // minimum value of 5 characters
+  check ('Username', 'Username is required').isLength({min: 5}),
+  check ('Username', 'Username contains non alphanumeric character - not allowed.').isAlphanumeric(),
+  // not empty
+  check('Password', 'Password is required').not().isEmpty(),
+  check ('Email', 'Email does not appear to be valid').isEmail()
+], (req, res) => {
+    // checks validation object for errors
+  let errors = validationResult(req);
 
-/* We’ll expect JSON in this format
-{
-  ID: Integer,
-  Username: String,
-  Password: String,
-  Email: String,
-  Birthday: Date
-}*/
+  if(!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array()});
+  } 
 
-app.post('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
-	Users.findOne({ Username: req.body.Username })
-	  .then((user) => {
-		if (user) {
-		  return res.status(400).send(req.body.Username + 'already exists');
-		} else {
-		  Users
-			.create({
-			  Username: req.body.Username,
-			  Password: req.body.Password,
-			  Email: req.body.Email,
-			  Birthday: req.body.Birthday
-			})
-			.then((user) =>{res.status(201).json(user) })
-		  .catch((error) => {
-			console.error(error);
-			res.status(500).send('Error: ' + error);
-		  })
-		}
-	  })
-	  .catch((error) => {
-		console.error(error);
-		res.status(500).send('Error: ' + error);
-	  });
+  let hashedPassword = Users.hashPassword(req.body.Password); // this hash the password before storing it in the mongoDB database
+  Users.findOne({ Username: req.body.Username })
+  .then((user) => {
+    if (user) {
+      return res.status(400).send(req.body.Username + ' already exists');
+    } else {
+      Users
+      .create({
+        Username: req.body.Username,
+        Password: hashedPassword,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then((user) =>{res.status(201).json(user) })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      })
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
   });
+});
   
   // Get all users / GET
   app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -146,35 +158,40 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) 
   });
 
 // Update a user's info, by username / UPDATE
-/* We’ll expect JSON in this format
-{
-  Username: String,
-  (required)
-  Password: String,
-  (required)
-  Email: String,
-  (required)
-  Birthday: Date
-}*/
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-	Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
-	  {
-		Username: req.body.Username,
-		Password: req.body.Password,
-		Email: req.body.Email,
-		Birthday: req.body.Birthday
-	  }
-	},
-	{ new: true }, // This line makes sure that the updated document is returned
-	(err, updatedUser) => {
-	  if(err) {
-		console.error(err);
-		res.status(500).send('Error: ' + err);
-	  } else {
-		res.json(updatedUser);
-	  }
+	app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
+	[ 
+	check ('Username', 'Username is required').isLength({min: 5}),
+	check ('Username', 'Username contains non alphanumeric character - not allowed.').isAlphanumeric(),
+	check('Password', 'Password is required').not().isEmpty(),
+	check ('Email', 'Email does not appear to be valid').isEmail()
+	], (req, res) => {
+
+	let errors = validationResult(req);
+
+	if(!errors.isEmpty()) {
+		return res.status(422).json({ errors: errors.array()});
+	} 
+
+	let hashedPassword = Users.hashPassword(req.body.Password);
+	Users.findOneAndUpdate({ Username: req.params.Username},
+		{ 
+		$set: {
+			Username: req.body.Username,
+			Password: hashedPassword,
+			Email: req.body.Email,
+			Birthday: req.body.Birthday
+		},
+		},
+		{new: true }, //This line makes sure that the updated document is returned
+		(err, updatedUser) => {
+		if(err) {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		} else {
+			res.json(updatedUser);
+		}
+		});
 	});
-  });
 
 
 // Add a movie to a user's list of favorites / UPDATE
@@ -238,6 +255,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
