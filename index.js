26f0ee8bf313ff5820f34d31f078ffc16ca53a22
Luisa-Inc <118922382+Luisa-Inc@users.noreply.github.com>
2022-12-29 21:05:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const Users = Models.User; //refer to the model names you defined in “models.j
 
 /*mongoose.connect('mongodb://localhost:27017/myFlixDB ', { useNewUrlParser: true, useUnifiedTopology: true });*/
 
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
