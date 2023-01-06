0fb1fd6d1fa48acf8285ac72360dc7548d8b613d
Luisa Ihle <luisa.ihle@gmail.com>
2023-01-06 13:03:44 +0100

    update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const Movies = Models.Movie; //refer to the model names you defined in “models
 const Users = Models.User; //refer to the model names you defined in “models.js”
 
 /*mongoose.connect('mongodb://localhost:27017/myFlixDB ', { useNewUrlParser: true, useUnifiedTopology: true });*/
-mongoose.connect('mongodb+srv://myflixluisaadmin:CF2023@myflixdb.7hxqnxa.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
